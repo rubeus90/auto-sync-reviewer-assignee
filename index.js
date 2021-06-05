62776621b56c4db7eaed98d768f2b71cb5d8ee55
Hong Ngoc Nguyen <ngocn@scribd.com>
2021-06-04 22:55:45 -0400

    Code reformat + add info log

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ async function execute() {
         const issue_number = context.issue.number
 
         // Fetch pull request info
-        const { data: pullRequest } = await octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number,
-        });
-        console.log("coucou")
-        console.log(context)
+        const { data: pullRequest } = await octokit.rest.pulls.get({owner, repo, pull_number});
         const assigneeLogins = pullRequest.assignees.map(assignee => assignee.login)
         const reviewerLogins = pullRequest.requested_reviewers.map(reviewer => reviewer.login)
 
@@ -29,47 +23,28 @@ async function execute() {
         excludeList.push(context.actor)
 
         // Add and remove reviewers/assignees when there's a difference between the 2 lists
+        core.info('Workflow triggered on action ' + context.payload.action)
         switch(context.payload.action) {
             case 'assigned':
             case 'unassigned':
                 const reviewersToAdd = assigneeLogins.filter(login => !excludeList.includes(login) && !reviewerLogins.includes(login))
                 if (reviewersToAdd.length > 0) {
-                    await octokit.rest.pulls.requestReviewers({
-                        owner,
-                        repo,
-                        pull_number,
-                        reviewers: reviewersToAdd,
-                    })
+                    await octokit.rest.pulls.requestReviewers({owner, repo, pull_number, reviewers: reviewersToAdd})
                 }
                 const reviewersToRemove = reviewerLogins.filter(login => !excludeList.includes(login) && !assigneeLogins.includes(login))
                 if (reviewersToRemove.length > 0) {
-                    await octokit.rest.pulls.removeRequestedReviewers({
-                        owner,
-                        repo,
-                        pull_number,
-                        reviewers: reviewersToRemove,
-                      })
+                    await octokit.rest.pulls.removeRequestedReviewers({owner, repo, pull_number, reviewers: reviewersToRemove})
                 }
                 break
             case 'review_requested':
             case 'review_request_removed':
                 const assigneesToAdd = reviewerLogins.filter(login => !excludeList.includes(login) && !assigneeLogins.includes(login))
                 if (assigneesToAdd.length > 0) {
-                    await octokit.rest.issues.addAssignees({
-                        owner,
-                        repo,
-                        issue_number,
-                        assignees: assigneesToAdd,
-                      })
+                    await octokit.rest.issues.addAssignees({owner, repo, issue_number, assignees: assigneesToAdd})
                 }
                 const assigneesToRemove = assigneeLogins.filter(login => !excludeList.includes(login) && !reviewerLogins.includes(login))
                 if (assigneesToRemove.length > 0) {
-                    await octokit.rest.issues.removeAssignees({
-                        owner,
-                        repo,
-                        issue_number,
-                        assignees: assigneesToRemove,
-                    })
+                    await octokit.rest.issues.removeAssignees({owner, repo, issue_number, assignees: assigneesToRemove})
                 }
                 break
         }
